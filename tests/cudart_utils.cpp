9cd7096e2ad0c455692c0f7f057b22a924952cec
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-07-13 05:09:17 -0700

    unit-tests for cudart-utils

--- tests/cudart_utils.cpp
+++ tests/cudart_utils.cpp
@@ -3,4 +3,19 @@
 
 namespace cuda {
 
+TEST_CASE("THROW") {
+  auto func = []() { THROW("This will throw an exception"); };
+  REQUIRE_THROWS_AS(func(), std::runtime_error);
+}
+
+TEST_CASE("ASSERT") {
+  auto func = [](bool pass) { ASSERT(pass, "This will try to assert"); };
+  REQUIRE_THROWS_AS(func(false), std::runtime_error);
+  REQUIRE_NOTHROW(func(true));
+}
+
+TEST_CASE("sync") {
+  REQUIRE_NOTHROW(sync(0));
+}
+
 }  // namespace cuda
